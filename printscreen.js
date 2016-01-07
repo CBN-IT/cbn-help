6db84d2368b6168729f7855e7f18563890b1fd92
Bogdan <bogdan.nourescu@cbn-it.ro>
2016-01-07 18:36:05 +0200

    function that makes a print-screen on shadyDom. ! does not work on shadow dom

--- printscreen.js
+++ printscreen.js
@@ -1,4 +1,185 @@
 function makePrintScreen(){
+	console.time("all");
+
+	function removeScripts(tag, tagname){
+		var scripts = tag.querySelectorAll(tagname);
+		var i = scripts.length;
+		while (i--) {
+			scripts[i].parentNode.removeChild(scripts[i]);
+		}
+	}
+	var linksWeakMap ={};
+	function cloneStyle(style, baseUri){
+		var clonedStyle = style.cloneNode(true);
+		var styleContent = style.innerHTML;
+		styleContent = styleContent.replace(/url\s*\([\s'"]*([^)\s'"]+)[\s'"]*\)/g, function () {
+			var url = arguments[1];
+			if (url.indexOf("http://") !== 0 &&
+				url.indexOf("https://") !== 0) {
+				var newUrl = baseUri.substring(0, baseUri.lastIndexOf("/") + 1) + url;
+				return arguments[0].replace(arguments[1], newUrl);
+			}
+		});
+		clonedStyle.innerHTML = styleContent;
+		return clonedStyle;
+	}
+	function copyStyleToHead(head, tag) {
+		var scripts = tag.querySelectorAll('link[rel="import"]');
+
+		for (var i in scripts) {
+			if (!scripts.hasOwnProperty(i)) {
+				continue;
+			}
+			var script = scripts[i].import.documentElement;
+			if (!linksWeakMap[script.baseURI]) {
+				linksWeakMap[script.baseURI] = true;
+				var styles = script.querySelectorAll('style:not([is="custom-style"])');
+				if (styles.length > 0) {
+					for (var k = 0; k < styles.length; k++) {
+						var style = styles[k];
+						//clone style the hardcore way to map relative urls to absolute urls
+						
+						head.appendChild(cloneStyle(style, script.baseURI));
+					}
+				}
+				copyStyleToHead(head, script);
+			}
+
+		}
+	}
+	function saveValueOfNativeTags(tag){
+		var inputs = tag.querySelectorAll("input");
+		for (var i = 0; i < inputs.length; i++) {
+			var input = inputs[i];
+			input.setAttribute("cbn-print-screen-value", input.value);
+		}
+	}
+	function removeValueOfNativeTags(tag){
+		var inputs = tag.querySelectorAll("[cbn-print-screen-value]");
+		for (var i = 0; i < inputs.length; i++) {
+			var input = inputs[i];
+			input.removeAttribute("cbn-print-screen-value");
+		}
+	}
+	function setValueOfNativeTags(tag){
+		var inputs = tag.querySelectorAll("input[cbn-print-screen-value]");
+		for (var i = 0; i < inputs.length; i++) {
+			var input = inputs[i];
+			input.setAttribute("value",input.getAttribute("cbn-print-screen-value"));
+		}
+	}
+
+	function saveScrollOfTags(tagParam){
+		var tags = tagParam.querySelectorAll("*");
+		for (var i = 0; i < tags.length; i++) {
+			var tag = tags[i];
+			if(tag.scrollTop>0){
+				tag.setAttribute("cbn-print-screen-scroll-top", tag.scrollTop);
+			}
+			if(tag.scrollLeft>0){
+				tag.setAttribute("cbn-print-screen-scroll-left", tag.scrollLeft);
+			}
+		}
+	}
+	function removeScrollOfTags(tag){
+		var elem = tag.querySelectorAll("[cbn-print-screen-scroll-top], [cbn-print-screen-scroll-left]");
+		for (var i = 0; i < elem.length; i++) {
+			var input = elem[i];
+			input.removeAttribute("cbn-print-screen-scroll-top");
+		}
+	}
+	function setScrollOfTags(){
+		var elem = document.querySelectorAll("[cbn-print-screen-scroll-top], [cbn-print-screen-scroll-left]");
+		for (var i = 0; i < elem.length; i++) {
+			var input = elem[i];
+			var scrollTop = input.getAttribute("cbn-print-screen-scroll-top");
+			var scrollLeft = input.getAttribute("cbn-print-screen-scroll-left");
+			if (scrollTop) {
+				input.scrollTop = scrollTop;
+			}
+			if (scrollLeft) {
+				input.scrollLeft = scrollLeft;
+			}
+		}
+		removeScrollOfTags(document);
+	}
+	saveValueOfNativeTags(document.documentElement);
+	saveScrollOfTags(document.documentElement);
+	var html = new DOMParser().parseFromString(document.documentElement.outerHTML, "text/html");
+	setValueOfNativeTags(html.documentElement);
+	removeValueOfNativeTags(document.documentElement);
+	removeValueOfNativeTags(html.documentElement);
+	removeScrollOfTags(document.documentElement);
+	
+	var head = html.head;
+	var body = html.body;
+	copyStyleToHead(head,document);
+	removeScripts(html.documentElement, "script");
+	removeScripts(html.documentElement, "template");
+	removeScripts(html.documentElement, "link");
+	var w = window.open('', "", "height="+window.innerHeight+",width="+window.innerWidth+",location=no,menubar=no,resizable=no,status=no,titlebar=no,toolbar=no");
+	w.document.open();
+	w.document.write("<!doctype html>"+html.documentElement.outerHTML+"<script>"+setScrollOfTags+removeScrollOfTags+"window.onload=setScrollOfTags;</script>");
+	w.document.close();
+	console.timeEnd("all");
+}
+
+
+function makePrintScreenVersion2(){
+	/*console.time("test");
+	var global = {};
+	var tagCount = 0;
+	function parseComputedStyle(parent) {
+		for (var i = 0; i < parent.children.length; i++) {
+			var child = parent.children[i];
+			tagCount++;
+			var x = getComputedStyle(child);
+			for (var j = 0; j < x.length; j++) {
+				var k = x[j] + ": " + x.getPropertyValue(x[j]);
+				if (global[k] == undefined) {
+					global[k] = [];
+				}
+				global[k].push(child);
+			}
+			parseComputedStyle(child)
+		}
+	}
+
+	function computeClasses(tag) {
+		parseComputedStyle(tag);
+		var styles = {
+			a: ""
+		};
+		var alphabet = "abcdefghijklmnopqrstuvwxyz";
+		var alphabetCounter = 0;
+		for (var i in global) {
+			if (!global.hasOwnProperty(i)) {
+				continue;
+			}
+			var g = global[i];
+			if (tagCount / 2 < g.length) {
+				//should be added to all elements
+				styles["a"] += i + ";\n";
+			} else {
+				var nextStyle = alphabet[parseInt(alphabetCounter / alphabet.length)] + alphabet[(alphabetCounter % alphabet.length)];
+				styles[nextStyle] = i + ";";
+				alphabetCounter++;
+				for (var j = 0; j < g.length; j++) {
+					var t = g[j];
+
+					if (t.applyClass == undefined) {
+						t.applyClass = "a";
+					}
+					t.applyClass += " " + nextStyle;
+				}
+			}
+		}
+		console.log(styles);
+	}
+	//computeClasses(document.body);
+	console.timeEnd("test");*/
+}
+function makePrintScreenJquery(){
 	/*$('#bugReport').hide();
 	 $('#bugReport').dialog("close");
 
@@ -43,80 +224,4 @@ function makePrintScreen(){
 	 "left": (mousePosition.x - 4),
 	 "z-index": 9999999
 	 }));*/
-
-
-	function removeScripts(tag, tagname){
-		var scripts = tag.querySelectorAll(tagname);
-		var i = scripts.length;
-		while (i--) {
-			scripts[i].parentNode.removeChild(scripts[i]);
-		}
-	}
-	var html = new DOMParser().parseFromString("<html>"+document.head.outerHTML+document.body.outerHTML+"</html>", "text/html");
-	var head = html.head;
-	var body = html.body;
-	removeScripts(head, "script");
-	removeScripts(body, "script");
-	removeScripts(head, "template");
-	removeScripts(body, "template");
-	removeScripts(head, "link");
-	removeScripts(body, "link");
-	removeScripts(body, "style");
-	console.time("test");
-	var global = {};
-	var tagCount = 0;
-	function parseComputedStyle(parent) {
-		for (var i = 0; i < parent.children.length; i++) {
-			var child = parent.children[i];
-			tagCount++;
-			var x = getComputedStyle(child);
-			for (var j = 0; j < x.length; j++) {
-				var k = x[j] + ": " + x.getPropertyValue(x[j]);
-				if (global[k] == undefined) {
-					global[k] = [];
-				}
-				global[k].push(child);
-			}
-			parseComputedStyle(child)
-		}
-	}
-
-	function computeClasses(tag) {
-		parseComputedStyle(tag);
-		var styles={
-			a:""
-		};
-		var alphabet = "abcdefghijklmnopqrstuvwxyz";
-		var alphabetCounter=0;
-		for(var i in global){
-			if(!global.hasOwnProperty(i)){
-				continue;
-			}
-			var g = global[i];
-			if (tagCount / 2 < g.length) {
-				//should be added to all elements
-				styles["a"] += i + ";\n";
-			} else {
-				var nextStyle = alphabet[parseInt(alphabetCounter / alphabet.length)] + alphabet[(alphabetCounter % alphabet.length)];
-				styles[nextStyle] = i + ";";
-				alphabetCounter++;
-				for(var j=0;j< g.length;j++){
-					var t = g[j];
-
-					if(t.applyClass==undefined){
-						t.applyClass = "a";
-					}
-					t.applyClass+=" "+nextStyle;
-				}
-			}
-		}
-		console.log(styles);
-	}
-	computeClasses(document.body);
-	console.timeEnd("test");
-	var w = window.open('', "", "location=no,menubar=no,resizable=no,status=no,titlebar=no,toolbar=no");
-	w.document.open();
-	w.document.write("<!doctype html>"+"<html>"+head.outerHTML+body.outerHTML+"</html>");
-	w.document.close();
-
 }
